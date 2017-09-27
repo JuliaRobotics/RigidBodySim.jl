b9eddcfe20c177678bbda0c19e8a765e2d54c69d
Twan Koolen <koolen.twan@gmail.com>
2017-09-27 15:11:59 -0400

    RigidBodyDynamicsDiffEqIntegration.jl generated files.
        license:  MIT
        authors:  Twan Koolen
        years:    2017
        user:     tkoolen
    
    Julia Version 0.6.0 [9036443]

---
+++ src/RigidBodyDynamicsDiffEqIntegration.jl
@@ -0,0 +1,5 @@
+module RigidBodyDynamicsDiffEqIntegration
+
+# package code goes here
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using RigidBodyDynamicsDiffEqIntegration
+using Base.Test
+
+# write your own tests here
+@test 1 == 2
