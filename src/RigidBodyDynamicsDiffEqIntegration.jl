250b756f5fcc259cd8f514f13f1a6933d36ac0bb
Twan Koolen <koolen.twan@gmail.com>
2017-09-27 17:57:07 -0400

    Add ODEProblem constructor method.

--- src/RigidBodyDynamicsDiffEqIntegration.jl
+++ src/RigidBodyDynamicsDiffEqIntegration.jl
@@ -1,5 +1,45 @@
 module RigidBodyDynamicsDiffEqIntegration
 
-# package code goes here
+using RigidBodyDynamics
+import DiffEqBase
+
+using RigidBodyDynamics: configuration_derivative! # TODO: export from RigidBodyDynamics
+
+function zero_control!(τ::AbstractVector, t, state::MechanismState)
+    τ[:] = 0
+end
+
+function DiffEqBase.ODEProblem(state::MechanismState{X, M, C}, tspan, control! = zero_control!) where {X, M, C}
+    result = DynamicsResult{C}(state.mechanism)
+    τ = similar(velocity(state))
+    x = state_vector(state) # TODO: Vector constructor
+
+    # TODO: renormalization of configuration vector
+    # TODO: running controller at a reduced rate
+    # TODO: ability to affect external wrenches
+
+    nq = num_positions(state)
+    nv = num_velocities(state)
+    ns = num_additional_states(state)
+
+    closed_loop_dynamics! = function (t, x, ẋ)
+        # TODO: unpack function in RigidBodyDynamics:
+            q̇ = view(ẋ, 1 : nq)
+            v̇ = view(ẋ, nq + 1 : nq + nv)
+            ṡ = view(ẋ, nq + nv + 1 : nq + nv + ns)
+
+        set!(state, x)
+        control!(τ, t, state)
+        dynamics!(result, state, τ)
+
+        configuration_derivative!(q̇, state)
+            v̇ .= result.v̇
+            ṡ .= result.ṡ
+
+            ẋ
+    end
+
+    DiffEqBase.ODEProblem(closed_loop_dynamics!, x, tspan)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,27 @@
 using RigidBodyDynamicsDiffEqIntegration
+using RigidBodyDynamics
+using OrdinaryDiffEq
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "compare to simulate" begin
+    srand(1)
+
+    urdf = Pkg.dir("RigidBodyDynamicsDiffEqIntegration", "test", "urdf", "Acrobot.urdf")
+    mechanism = parse_urdf(Float64, urdf)
+
+    state = MechanismState(mechanism)
+    rand!(state)
+    x0 = state_vector(state) # TODO: Vector constructor
+
+    final_time = 5.
+    problem = ODEProblem(state, (0., final_time))
+    sol = solve(problem, Vern7(), abs_tol=1e-10, dt=0.05)
+
+    set!(state, x0)
+    ts, qs, vs = RigidBodyDynamics.simulate(state, final_time)
+
+    @show [qs[end]; vs[end]]
+    @show sol[end]
+
+    @test [qs[end]; vs[end]] ≈ sol[end] atol = 1e-2
+end
