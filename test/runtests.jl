32c493568d3874fb07d0e8aab0eb73fc60b16692
Twan Koolen <koolen.twan@gmail.com>
2017-09-28 16:31:00 -0400

    Slightly more exciting example in visualization test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,20 +25,14 @@ using Base.Test
 end
 
 @testset "visualizer callback" begin
-    srand(1)
-    urdf = Pkg.dir("RigidBodyDynamicsDiffEqIntegration", "test", "urdf", "Acrobot.urdf")
-    mechanism = parse_urdf(Float64, urdf)
-
+    mechanism = rand_tree_mechanism(Float64, [Revolute{Float64} for i = 1 : 30]...)
     state = MechanismState(mechanism)
-    rand!(state)
 
-    # DrakeVisualizer.new_window(); sleep(3)
-    vis = Visualizer()
-    setgeometry!(vis, mechanism, parse_urdf(urdf, mechanism))
+    # DrakeVisualizer.new_window(); sleep(1)
+    vis = Visualizer(mechanism; show_inertias = true)
     settransform!(vis, state)
 
-    problem = ODEProblem(state, (0., 100.))
+    problem = ODEProblem(state, (0., 1.))
     vis_callback = VisualizerCallback(state, vis)
-
-    sol = solve(problem, Vern7(), abs_tol = 1e-10, dt = 0.05, callback = vis_callback)
+    sol = solve(problem, RK4(), adaptive = false, dt = 1e-4, callback = vis_callback)
 end
